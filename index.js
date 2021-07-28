291081d57a60bca76b0c39c6b898b7952c37f1ff
近藤大翔 <taisho90@icloud.com>
2021-07-28 11:23:10 +0900

    implement preview

--- index.js
+++ index.js
@@ -4,17 +4,19 @@ import { Context } from "./class/context.js";
 import { update } from "./update.js";
 import { preview } from "./preview.js";
 
-const rootCanvas = $('#rootCanvas')
+const previewCanvas = $('#previewCanvas')
+const updateCanvas = $('#updateCanvas')
 
 const url = 'hogehoge.url'
-const width = 100
+const width = 150
 const dark_color = "rgba(0,0,255,1)"
 const light_color = "rgba(255,255,255,1)"
-const image = "../public/bowl.png"
+const logo = "../public/bowl.png"
 
-const instance = new Instance(rootCanvas)
-const properties = new Properties(width, url, light_color, dark_color, image)
+const preview_instance = new Instance(previewCanvas)
+const update_instance = new Instance(updateCanvas)
+const properties = new Properties(width, url, light_color, dark_color, logo)
 const context = new Context()
 
-preview(instance, properties, context)
-update(instance, properties, context)
+preview(preview_instance, properties, context)
+update(update_instance, properties, context)
--- preview.js
+++ preview.js
@@ -1,4 +1,14 @@
+// QRCodeが使えないので、静的リンクでなんとかする。
+// サイズとロゴだけ確認できるようにはできるかな
 export const preview = function(instance, properties) {
-    const button = $("<div>this space </div>")
-    instance.canvas.append(button)
+    const margin = 4
+
+    // 上下左右のmarginを省く
+    const width = properties.width - margin * 2
+    var qr = new Image(width, width)
+    qr.style.paddingLeft = "4px"
+    qr.style.paddingTop  = "4px"
+
+    qr.src = "https://s3.amazonaws.com/appforest_uf/f1627437102891x317756865227791360/qr-code.png"
+    instance.canvas.append(qr)
 }
